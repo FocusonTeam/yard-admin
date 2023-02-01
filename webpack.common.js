b2e55e1bbefe5021c59c91b058cc5eb6e59d8bdd
sr.lee@thebala.co.kr <sr.lee@theyard.co.kr>
2023-02-01 13:39:05 +0900

    Fix : netlify.toml

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 const { ProvidePlugin } = require("webpack");
+const dotenv = require('dotenv');
 
 module.exports = {
   entry: "./src/index.tsx",
