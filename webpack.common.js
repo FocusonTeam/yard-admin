967309bbc769449f9d049ba284d2ed1f53ab4205
sr.lee@thebala.co.kr <sr.lee@theyard.co.kr>
2023-02-03 12:55:35 +0900

    Feat : Add Proxy Address

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,11 @@ dotenv.config();  //.env 파일 파싱한 객체 리턴되어 process.env로 세
 
 module.exports = {
   entry: "./src/index.tsx",
+  // devSever: {
+  //   proxy: {
+  //     '/graphql': 'adminserver.theyard.co.kr'
+  //   }
+  // },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
     modules: [path.resolve(__dirname, "src"), "node_modules"],
@@ -70,4 +75,5 @@ module.exports = {
       process: 'process/browser.js',
     })
   ],
+  
 };
