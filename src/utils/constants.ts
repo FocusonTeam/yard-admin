558ffbb569e0a5ae6c3d04493c3e6cf2296f128e
sr.lee@thebala.co.kr <sr.lee@theyard.co.kr>
2023-04-07 16:07:01 +0900

    Fix : Constant Base URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const ARTICLES_IMAGES_REF = 'images/articles/temp';
 export const AREA_IMAGES_REF = 'images/area/temp';
-export const CLOUD_STORAGE_BASE_URL = process.env.REACT_APP_AWS_S3_URL;
+export const CLOUD_STORAGE_BASE_URL = process.env.REACT_APP_AWS_S3_PROD_URL;
+//export const CLOUD_STORAGE_DEV_URL = process.env.REACT_APP_AWS_S3_URL;
 export const CLOUD_STORAGE_PROD_URL = process.env.REACT_APP_AWS_S3_PROD_URL;
